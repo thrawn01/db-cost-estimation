b02904e6f3522b521197afedee1dc23fbe34c97c
Derrick J. Wippler <thrawn01@gmail.com>
2024-05-01 14:28:25 -0500

    Added simple-get.sql

---
+++ simple-get.sql
@@ -0,0 +1,18 @@
+\set aid random(1, 2000000)
+
+SELECT name,
+       description,
+       sku,
+       on_back_order,
+       in_stock,
+       image_urls,
+       discount_codes,
+       categories,
+       price,
+       weight,
+       length,
+       width,
+       height,
+       is_available
+FROM products
+WHERE id = :aid

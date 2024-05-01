d3b1e9e7f9987bdcfe6286d184aa01ac712cd5c9
Derrick J. Wippler <thrawn01@gmail.com>
2024-05-01 14:37:59 -0500

    Fixed formating of simple queries

--- simple-page-by-id.sql
+++ simple-page-by-id.sql
@@ -1,20 +1,4 @@
 \set aoffset random(1, 2000000)
 
-SELECT name,
-       description,
-       sku,
-       on_back_order,
-       in_stock,
-       image_urls,
-       discount_codes,
-       categories,
-       price,
-       weight,
-       length,
-       width,
-       height,
-       is_available
-FROM products
-WHERE id > :aoffset
-ORDER by id
-LIMIT 10;
+SELECT name, description, sku, on_back_order, in_stock, image_urls, discount_codes, categories, price, weight,
+       length, width, height, is_available FROM products WHERE id > :aoffset ORDER by id LIMIT 10;

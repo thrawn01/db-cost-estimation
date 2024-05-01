9f48a32409a375e977451cd725ba5c47867d5b75
Derrick J. Wippler <thrawn01@gmail.com>
2024-05-01 14:24:42 -0500

    Updated simple queries

--- simple-page-by-offset.sql
+++ simple-page-by-offset.sql
@@ -1,3 +1,18 @@
 \set aoffset random(1, 2000000)
 
-SELECT p.* FROM products p OFFSET :aoffset LIMIT 10;
+SELECT name,
+       description,
+       sku,
+       on_back_order,
+       in_stock,
+       image_urls,
+       discount_codes,
+       categories,
+       price,
+       weight,
+       length,
+       width,
+       height,
+       is_available
+FROM products
+OFFSET :aoffset LIMIT 10;
--- simple-pgbench-propagate.sql
+++ simple-pgbench-propagate.sql
@@ -15,3 +15,5 @@ VALUES ('This is a Product Name',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
         'sku-abu-2024-0001', :abackorder, :ainstock, 'https://example.com/img_1.png, https://example.com/img_2.png',
         'DISC2345,DISC2345', 'Category 1,Category 2', :aprice, :acost, :aweight, :alength, :awidth, :aheight, true);
+
+COMMIT;

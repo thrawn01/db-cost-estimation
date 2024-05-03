ff78dc3ddd5ade8b61b7b8c1c7d6760838b86603
Derrick J. Wippler <thrawn01@gmail.com>
2024-05-03 08:19:18 -0500

    Added array_to_string() to complex-only-joins.sql

--- complex-only-joins.sql
+++ complex-only-joins.sql
@@ -1,7 +1,8 @@
 \set aoffset random(1, 1000)
 
 SELECT p.*,
-       array_to_string(array_agg(dc.name), ',') AS discount_codes
+       array_to_string(array_agg(dc.name), ',')      AS discount_codes,
+       array_to_string(array_agg(pi.image_url), ',') AS image_urls
 FROM products p
          JOIN
      categories c ON p.id = c.product_id
@@ -9,5 +10,7 @@ FROM products p
      inventory i ON p.id = i.product_id
          JOIN
      discount_codes dc ON p.id = dc.product_id
+         JOIN
+     product_images pi ON p.id = pi.product_id
 GROUP BY p.id, c.name
 OFFSET :aoffset LIMIT 10;

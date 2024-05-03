91994f611df3166e655af3ec79d335285f24318d
Derrick J. Wippler <thrawn01@gmail.com>
2024-05-03 09:29:13 -0500

    No longer using OFFSET in complex-only-joins.sql

--- complex-only-joins.sql
+++ complex-only-joins.sql
@@ -3,7 +3,7 @@
 SELECT p.*,
        array_to_string(array_agg(dc.name), ',')      AS discount_codes,
        array_to_string(array_agg(pi.image_url), ',') AS image_urls,
-       array_to_string(array_agg(c.name), ',') AS categories
+       array_to_string(array_agg(c.name), ',')       AS categories
 FROM products p
          JOIN
      categories c ON p.id = c.product_id
@@ -14,5 +14,6 @@ FROM products p
          JOIN
      product_images pi ON p.id = pi.product_id
 WHERE p.id > :aoffset
-GROUP BY p.id, c.name
-ORDER by id LIMIT 10;
+GROUP BY p.id
+ORDER by id
+LIMIT 10;
